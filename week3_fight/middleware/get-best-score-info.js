5c76c515bad46b5b759f812ddf95ef518e2f0a1e
Do-ho <hong7511059@koreatech.ac.kr>
2020-08-14 18:11:50 +0900

    Init Week3_fight

---
+++ week3_fight/middleware/get-best-score-info.js
@@ -0,0 +1,31 @@
+const User = require("../models/user");
+const sequelize = require("../util/database");
+const School = require("../models/school");
+
+exports.getBestScoreUsers = async (req, res, next) => {
+  const topUsers = await User.findAll({
+    order: [["score", "DESC"]],
+    limit: 3,
+  });
+
+  const topSchools = await User.findAll({
+    attributes: [
+      "schoolId",
+      [sequelize.fn("sum", sequelize.col("score")), "totalScore"],
+    ],
+    
+    group: ["schoolId"],
+    order: sequelize.literal("totalScore DESC"),
+    limit: 3,
+  });
+
+  for (school of topSchools) {
+    const eachSchool = await School.findByPk(school.schoolId);
+    school.schoolName = eachSchool.name;
+    school.schoolType = eachSchool.type;
+  }
+
+  req.topUsers = topUsers;
+  req.topSchools = topSchools;
+  next();
+};
